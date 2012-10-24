18f0e9eb2970681377d4ce6e1cb368919ee6952a
Scot Dalton <scotdalton@gmail.com>
2012-10-24 13:50:13 -0400

    Debugging

--- lib/compass-rails/railties/3_1.rb
+++ lib/compass-rails/railties/3_1.rb
@@ -72,6 +72,7 @@ puts "logical_path: #{logical_path}"
 puts "Rails.application.assets: #{Rails.application.assets}"
 puts "Rails.application.assets: #{Rails.application.assets.instance_variable_get(:@environment)}"
 put "attributes: #{Rails.application.assets.send(:attributes_for, logical_path).inspect}"
+put "search paths: #{Rails.application.assets.send(:attributes_for, logical_path).search_paths.inspect}"
           asset         = Rails.application.assets.find_asset(logical_path)
           target        = File.join(Rails.public_path, Rails.application.config.assets.prefix, asset.digest_path)
 puts "target: #{target}"
